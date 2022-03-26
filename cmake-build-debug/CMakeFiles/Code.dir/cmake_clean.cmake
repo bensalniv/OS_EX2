cc1d3bc5d1168612f075e6b72bdf429dbd5e594c
niv.bensalmon <niv.bensalmon@mail.huji.ac.il>
2022-03-26 11:40:48 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/Code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code.dir/library.c.o"
+  "libCode.a"
+  "libCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
